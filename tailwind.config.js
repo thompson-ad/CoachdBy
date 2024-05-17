5bf0425e9d2506ac32681e3fa4a1ffd31487bdba
Aaron Thompson <thompson_ad@outlook.com>
2024-05-17 11:01:09 +0100

    set up nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require("./src/ui/colors");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+  plugins: [],
+};
